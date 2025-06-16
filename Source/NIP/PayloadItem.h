5319fb269fe923c5ec72c83c159c1c5209e415f7
Stanislav <doudoladov_stanislav@mail.ru>
2025-06-16 08:11:40 +0300

    UI

---
+++ Source/NIP/PayloadItem.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "PayloadItem.generated.h"
+
+UCLASS()
+class NIP_API UPayloadItem : public UObject
+{
+	GENERATED_BODY()
+	
+};
