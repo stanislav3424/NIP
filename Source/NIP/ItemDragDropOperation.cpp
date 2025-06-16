8e7dd6f33b68a072a570d922d88090f1304b9213
Stanislav <doudoladov_stanislav@mail.ru>
2025-06-16 15:15:05 +0300

    1

---
+++ Source/NIP/ItemDragDropOperation.cpp
@@ -0,0 +1,2 @@
+#include "ItemDragDropOperation.h"
+
---
+++ Source/NIP/ItemDragDropOperation.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/DragDropOperation.h"
+#include "ItemDragDropOperation.generated.h"
+
+UCLASS()
+class NIP_API UItemDragDropOperation : public UDragDropOperation
+{
+	GENERATED_BODY()
+	
+};
