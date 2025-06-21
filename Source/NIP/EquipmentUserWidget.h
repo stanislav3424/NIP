c712a257b328b72cc864834d867cda6907b6ca66
Stanislav <doudoladov_stanislav@mail.ru>
2025-06-21 14:26:16 +0300

    1

---
+++ Source/NIP/EquipmentUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseUserWidget.h"
+#include "EquipmentUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NIP_API UEquipmentUserWidget : public UBaseUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/NIP/MapGeneration.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MapGeneration.h"
+
+// Sets default values
+AMapGeneration::AMapGeneration()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMapGeneration::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMapGeneration::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
