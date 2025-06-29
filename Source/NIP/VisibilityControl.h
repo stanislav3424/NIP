58e190f33159c7bc94bf9cdf74a6bb4343c95e75
Stanislav <doudoladov_stanislav@mail.ru>
2025-06-29 09:38:49 +0300

    й

---
+++ Source/NIP/VisibilityControl.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "VisibilityControl.generated.h"
+
+UCLASS()
+class NIP_API UVisibilityControl : public UAIPerceptionComponent
+{
+	GENERATED_BODY()
+	
+};
